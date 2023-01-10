b6e02f459982affc81ae9c9f3402f3eefe945883
Serhii Bilyk <serhii.bilyk@intive.com>
2023-01-10 10:23:39 +0100

    wip

--- 0_future_features.ts
+++ 0_future_features.ts
@@ -1,12 +1,25 @@
-declare function inference1<const T>(data: T): T;
+/**
+ * FIRST
+ */
+const inference1=<const T>(data: T)=>data
 
 const test1 = inference1({ a: 1 });
-//    ^?
 
+
+
+/**
+ * SECOND
+ */
 const inference2 = <T,>(data: T) => data
 
 const test2 = inference2({ a: 1 })
 
+
+
+
+/**
+ * THIRD
+ */
 const inference3 = <Value extends number, T extends Record<string, Value>>(data: T) => data
 
 const test3 = inference3({ a: 1 })
